630b060030c7fc38a60fccb1d1ff4a70e31e7821
1976udit <165473837+1976udit@users.noreply.github.com>
2024-12-25 11:06:07 +0000

    some

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,8 +1,22 @@
 import userModel from '../models/user.models.js'
-import {ValizationResult} from "express-validator"
+import {ValidationResult} from "express-validator"
+import createUser from '../services/user.service.js'
 
 async function registerUser(req,res,next) {
-   const error = ValizationResult.
+   const errors = ValidationResult(req)
+   if(!errors.isEmpty()){
+      return res.status(400).json({errors : errors.array()})
+   }
+
+   const {firstName, lastName, email, password} = req.body;
+   const hashedPassword = await userModel.hashPassword(password);
+
+   const user = await createUser({
+      firstName,
+      lastName,
+      email,
+      password : hashedPassword
+   })
 }
 
 export default registerUser;
